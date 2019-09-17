3383d1f27af7df8fd545331a2cab1a975b02baaa
Nagaraju Dachepally <nagaraju.dachepally@nagaraju-macbook.home>
2019-09-16 20:04:58 -0500

    longest common prefix 1st way of doing

---
+++ longest_common_prefix1.py
@@ -0,0 +1,41 @@
+"""
+this is a word based method to find the longest
+common prefix.
+"""
+
+
+def return_prefix(str1, str2):
+    """
+    compares two strings and returns common prefix
+    :param str1: string1
+    :param str2: string2
+    :return: string
+    """
+    l1 = len(str1)
+    l2 = len(str2)
+    i, j = 0, 0
+    prefix = ''
+    while i < l1 and j < l2:
+        if str1[i] != str2[j]:
+            break
+        prefix = prefix+str1[i]
+        i = i+1
+        j = j+1
+    return prefix
+
+
+def common_prefix_main(li):
+    """
+    list of strings
+    :param li:
+    :return:
+    """
+    prefix = li[0]
+    for i in range(1, len(li)):
+        prefix = return_prefix(prefix, li[i])
+    return prefix
+
+
+if __name__ == "__main__":
+    strings = ['flower', 'floor', 'flaws']
+    cp = common_prefix_main(strings)
